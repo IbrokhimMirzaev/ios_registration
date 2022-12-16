0bfb97a7934c4b87dfb81b2d1f7205126e0871f9
IbrokhimMirzaev <mirzaevibrokhim@gmail.com>
2022-12-16 20:47:01 +0500

    First UI completed

---
+++ app/src/main/java/com/example/ios_registration/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.ios_registration
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.first)
+    }
+}
